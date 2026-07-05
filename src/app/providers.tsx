ad3170dd3b5c40d34db16e758aded79d9298dd42
saifkhaled6 <khaled.saifullah@cefalo.com>
2026-07-05 18:05:54 +0600

    query client set up

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,19 @@
 'use client';
 
+import { useState } from 'react';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import StoreProvider from './StoreProvider';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <StoreProvider>
-      <AntdRegistry>{children}</AntdRegistry>
+      <QueryClientProvider client={queryClient}>
+        <AntdRegistry>{children}</AntdRegistry>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </StoreProvider>
   );
 }
